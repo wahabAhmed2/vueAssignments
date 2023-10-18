c7b23389e22f0714921a371725f75ca16cb27fe5
wahabAhmed2 <wahabahmed@folio3.com>
2023-10-18 17:15:50 +0500

    first assignment completed

---
+++ src/components/TodoComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>{{ message }}</h2>
+    <input v-model="inputValue" @keyup.enter="addItem" placeholder="Enter a new item">
+    <button @click="addItem">Submit</button>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">{{ item }} <button @click="deleteItem(index)">delete</button></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Todo List',
+      inputValue: '',
+      items: [], 
+    };
+  },
+  methods: {
+    addItem() {
+      if (this.inputValue) {
+        this.items.push(this.inputValue);
+        this.inputValue = '';
+      }
+    },
+    deleteItem(index){
+      this.items.splice(index,1)
+    }
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
